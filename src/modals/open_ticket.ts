1a1f55d9d09b62b839233b0da41f82e9b8d84743
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-02-28 22:12:47 -0800

    Update open_ticket.ts

--- src/modals/open_ticket.ts
+++ src/modals/open_ticket.ts
@@ -15,23 +15,24 @@ export default new ModalCommand({
         let supportRoleOnly = false;
         let threadStarterMember = await client.guilds.cache.get(interaction.guild_id).members.fetch(threadStarter);
 
+        const promptedQuestions = [
+            `Topic`,
+            `Server ID`,
+            `Describe in detail what you need help with`
+        ];
+        let questionResponses:PromptedQuestion[] = promptedQuestions.map((question, index) => {
+            return {
+                question,
+                response:interaction.data.components[index]?.components[0]?.value || "Not provided"
+            }
+        });
+
         client.createSupportThread({
-            shortDesc:`${threadStarterMember.nickname || threadStarterMember.user.tag}`, 
+            shortDesc:questionResponses[0].response, 
             userId:threadStarter, 
             privateTicket:supportRoleOnly
         })
         .then(channel => {                
-            const promptedQuestions = [
-            `Topic`,
-            `Server ID`,
-            `Describe in detail what you need help with`
-            ];
-            let questionResponses:PromptedQuestion[] = promptedQuestions.map((question, index) => {
-                return {
-                    question,
-                    response:interaction.data.components[index]?.components[0]?.value || "Not provided"
-                }
-            });
             (client.channels.cache.get(channel.id) as ThreadChannel).send({
             "content":`${supportRoleOnly?"\n:lock: *This is a private ticket, so only staff may reply.*":"\n:unlock: *This is a public ticket, everyone may view and reply to it..*"}\n\nHey <@${threadStarter}>, <@&${config.supportRoleId}> will be with you as soon as they're able to. Here are the responses you gave in the prompt:\n\n${questionResponses.map(q => `**${q.question}:**\n${q.response}`).join("\n\n")}\n\n*(Disclaimer: You may not receive an answer instantly. Many of us have lives outside of Discord and will respond whenever we're able to, whenever that is.)*`,
             "components":[
